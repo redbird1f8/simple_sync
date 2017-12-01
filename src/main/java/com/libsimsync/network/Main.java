1142f60a10ef0fce4a7f05c3c217fad9c224f465
Knst1 <knst@live.ru>
2017-12-01 21:40:35 +0300

    network 01.12

--- src/main/java/com/libsimsync/network/Main.java
+++ src/main/java/com/libsimsync/network/Main.java
@@ -1,19 +1,36 @@
 package com.libsimsync.network;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+import static java.lang.Thread.sleep;
 
         class Main {
+
             public static void main(String[] args) throws InterruptedException {
+
                 Peer first =  new Peer();
                 Peer second = new Peer();
                 Peer third  = new Peer();
 
-                first.listen(9100);
-                second.connect("localhost",9100);
-                third.connect("localhost",9100);
+                first.listen(61005);
+/*
+                third.connect("localhost",61005);
+                second.connect("localhost",61005);
+
+                sleep(200);
+                //first.sendObject(new Command(0,new String("ololol")));
+                first.sendStr("everything is working");
+                first.sendCommand(0,"yes, it is");
+                second.sendStr("wow");*/
 
             }
         }

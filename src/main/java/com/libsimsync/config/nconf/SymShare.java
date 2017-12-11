e27318f171e6307df811d2901f3d500a6dd93c63
redbird <redbirda47@gmail.com>
2017-12-11 21:51:32 +0300

    Added new config classes.

---
+++ src/main/java/com/libsimsync/config/nconf/SymShare.java
@@ -0,0 +1,43 @@
+package com.libsimsync.config.nconf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SymShare {
+    List<SyncDevice> acceptedDevices;
+    String rootPath;
+
+    public SymShare(){
+        acceptedDevices = new ArrayList<>();
+        rootPath = System.getProperty("user.dir"); //??
+    }
+
+    public SymShare(String root){
+        rootPath = root;
+        acceptedDevices = new ArrayList<>();
+    }
+
+    public SymShare(String root, List<SyncDevice> devices){
+        rootPath = root;
+        acceptedDevices = devices;
+    }
+
+    public List<SyncDevice> getDevices(){
+        return acceptedDevices;
+    }
+
+    public String getRootPath() {
+        return rootPath;
+    }
+
+
+    public void setDevices(List<SyncDevice> acceptedDevices) {
+        this.acceptedDevices = acceptedDevices;
+    }
+
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+
+}

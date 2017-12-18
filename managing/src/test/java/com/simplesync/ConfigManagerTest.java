79232de14fb15714f8527de3ddf0950e0cea497c
Your Name <your@email.address>
2017-12-19 02:57:28 +0300

    tested Configmanager

--- managing/src/test/java/com/simplesync/ConfigManagerTest.java
+++ managing/src/test/java/com/simplesync/ConfigManagerTest.java
@@ -1,10 +1,15 @@
 package com.simplesync;
 
 import com.simplesync.nconf.SymShare;
+import com.simplesync.nconf.SyncDevice;
 import org.junit.Test;
+
+import javax.swing.*;
+
 import static org.mockito.Mockito.*;
 
 import java.io.OutputStream;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,30 +18,30 @@ import static org.junit.Assert.*;
  */
 public class ConfigManagerTest {
 
-    @Test
-    public void setDefaultConfig() throws Exception {
-        SymShare mock = mock(SymShare.class);
-
-    }
-
-    @Test
-    public void changeDirectory() throws Exception {
-    }
-
-    @Test
-    public void addDevice() throws Exception {
-    }
-
-    @Test
-    public void deleteAllDevices() throws Exception {
-    }
-
-    @Test
-    public void applyConfig() throws Exception {
-    }
 
     @Test
     public void containsDevice() throws Exception {
+
+        List<SyncDevice> devices = ConfigManager.getSymShare().getDevices();
+
+        SyncDevice td1 = new SyncDevice("n1","ip1");
+        SyncDevice td2 = new SyncDevice("n2","ip2");
+        SyncDevice td3 = new SyncDevice("n3","ip3");
+
+        SyncDevice tempDevice  = new SyncDevice("n1","ip1");
+
+        devices.add(td1);
+        devices.add(td2);
+        devices.add(td3);
+
+       boolean actual =  ConfigManager.containsDevice(tempDevice);
+       boolean expected = true;
+       assertEquals(expected,actual);
+
+       devices.remove(td1);
+       devices.remove(td2);
+       devices.remove(td3);
+
     }
 
 }
